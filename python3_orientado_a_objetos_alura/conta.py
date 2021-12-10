78d913ce311e184e412c0c2462cccdc8e833a6a2
Willian Alves Fonseca <willian.alves2109@gmail.com>
2021-12-10 02:42:00 -0300

    Finalizando 3º curso de python alura

--- python3_orientado_a_objetos_alura/conta.py
+++ python3_orientado_a_objetos_alura/conta.py
@@ -9,17 +9,21 @@ class conta:
         self.__saldo = saldo
         self.__limite = limite
 
-    def get_limite(self):
+    @property
+    def limite(self):
         return self.__limite
 
-    def set_limite(self, valor):
+    @limite.setter
+    def limite(self, valor):
         self.__limite = valor
 
-    def get_saldo(self):
+    @property
+    def saldo(self):
         return self.__saldo
 
-    def set_saldo(self, valor):
-        self.__saldo == valor
+    @saldo.setter
+    def saldo(self, valor):
+        self.__saldo += valor
 
     def extrato(self):
         print("Cliente: {}".format(self.__titular))
@@ -33,10 +37,20 @@ class conta:
         print("o valor R${} foi depositado com sucesso!".format(valor))
         print("=================================")
 
+    def __pode_sacar(self, valor_a_sacar):
+        valor_disponivel = self.__saldo + self.__limite
+        return valor_a_sacar <= valor_disponivel
+
     def sacar(self, valor):
-        self.__saldo -= valor
-        print("o valor R${} foi sacado com sucesso!".format(valor))
-        print("=================================")
+        if(self.__pode_sacar(valor)):
+            self.__saldo -= valor
+            print("o valor R${} foi sacado com sucesso!".format(valor))
+            print("=================================")
+        else:
+            print(
+                "o valor R${} não pode ser sacado, valor acima do limite!".format(valor))
+            print("O saldo atual é de R${}".format(self.__saldo))
+            print("=================================")
 
     def transferir(self, valor, destino):
         self.sacar(valor)
@@ -44,13 +58,29 @@ class conta:
         print("o valor R${} foi transferido com sucesso!".format(valor))
         print("=================================")
 
+    # METODOS ESTATICOS (NÃO É USUAL ESTE TIPO DE IMPLEMENTAÇÃO)
+    @staticmethod
+    def codigo_banco():
+        return "001"
+
+    @staticmethod
+    def codigos_bancos():
+        return {'BB': '001', 'Caixa': '104', 'Bradesco': '237'}
+
 
 if __name__ == "__main__":
-    conta1 = conta(322, "Willian Alves Fonseca", 250.00, 2000.00)
-    conta2 = conta(222, "Willian Alves Fonseca", 5000.00, 10000.00)
+    conta1 = conta(322, "Will Fonseca", 250.00, 2000.00)
+    conta2 = conta(222, "Willian Fonseca", 5000.00, 10000.00)
 
     conta1.transferir(100, conta2)
 
     conta1.extrato()
 
     conta2.extrato()
+
+    conta1.limite = 2000
+    conta1.saldo = 3000
+
+    conta1.sacar(200000)
+
+    conta1.extrato()
